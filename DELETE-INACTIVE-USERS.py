09e982e96639232b3572515eccda56064e62ec8e
Vaibhav Shukla <52421094+mr-vaibh@users.noreply.github.com>
2022-06-13 21:25:48 +0530

    Add files via upload

--- DELETE-INACTIVE-USERS.py
+++ DELETE-INACTIVE-USERS.py
@@ -1,45 +1,49 @@
 from zk import ZK, const
 
-conn = None
-# create ZK instance
 
-# For eg: 192.168.5.111
-IP = input("Enter machine IP: ")
+def delete_users(IP):
+    conn = None
 
-zk = ZK(IP, port=4370, timeout=60, password=0, force_udp=False, ommit_ping=False)
-try:
-    # connect to device
-    conn = zk.connect()
+    # create ZK instance
+    zk = ZK(IP, port=4370, timeout=60, password=0, force_udp=False, ommit_ping=False)
 
-    total_users = conn.get_users()
+    try:
+        # connect to device
+        conn = zk.connect()
 
-    # make a user map
-    user_map = {}
-    for user in total_users:
-        user_map[user.user_ud] = user.uid
+        total_users = conn.get_users()
 
-    print(f"Total users - {len(total_users)}")
+        # make a user map
+        user_map = {}
+        for user in total_users:
+            user_map[user.user_ud] = user.uid
 
-    confirm = input(
-        "Are you sure you want to delete all the employees listed in employee_inactive.txt? [y/n]")
+        print(f"Total users - {len(total_users)}")
 
-    with open('inactive_employees.txt', 'r') as file:
-        data = file.readlines()
-        print(f"Users to delete - {len(data)}")
-        for userid in data:
-            user_id = userid.replace('\n', '')
-            try:
-                conn.delete_user(uid=user_map[user_id])
-            except Exception as error:
-                print(error)
-                continue
+        with open('inactive_employees.txt', 'r') as file:
+            data = file.readlines()
+            print(f"Users to delete - {len(data)}")
+            for userid in data:
+                user_id = userid.replace('\n', '')
+                try:
+                    conn.delete_user(uid=user_map[user_id])
+                except Exception as error:
+                    print(error)
+                    continue
 
-            print(f'User {user_id} deleted')
+                print(f'User {user_id} deleted')
 
-    print("User deleted !")
+        print("User deleted !")
 
-except Exception as e:
-    print("Process terminate : {}".format(e))
-finally:
-    if conn:
-        conn.disconnect()
+    except Exception as e:
+        print("Process terminate : {}".format(e))
+    finally:
+        if conn:
+            conn.disconnect()
+
+# Code starts here
+with open('machine_list.txt', 'r') as file:
+    data = file.readlines()
+    for ip in data:
+        ip = ip.replace('\n', '')
+        delete_users(ip)
